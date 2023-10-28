04b3210e986218f5396e6c6d376e0a63e4f69254
Blagues <kenzoheijman@hotmail.com>
2023-10-28 19:10:06 +0200

    using even less memory

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use std::collections::VecDeque;
 const L: usize = 9;
 const C: usize = 9;
 
+
+
 struct State {
-    board: [[u8; C]; L],
-    pos: [[u16; C]; L],
+    board: [[u16; C]; L],
+    // we save the board position as an u16 with the first bit being 1 if it is a filled in position.
+    // And the next bits being the number that is filled in.
+    // If we have an empty square (0 as first bit), we save the next 9 bits for the numbers that are
+    // possible or not (1=yes) and the last 6 bits for the nr of possibilities.
 }
 
 impl State {
@@ -15,8 +20,10 @@ impl State {
 
         for l in 0..L {
             for c in 0..C {
-                let length = self.pos[l][c] & 63; // Extract last 6 bits
-                if length < min_value && length > 0 {
+                if self.is_filled_square(l, c) {continue};
+
+                let length = self.board[l][c] & 63; // Extract last 6 bits
+                if length < min_value {
                     min_value = length;
                     min_position = (l, c);
                 }
@@ -26,42 +33,52 @@ impl State {
         min_position
     }
 
-    fn get_options_for_cell(&self, l: usize, c: usize) -> Vec<u8> {
-        if l >= L || c >= C {
+    fn is_filled_square(&self, l: usize, c: usize) -> bool {
+        let mask: u16 = 0b1000000000000000;
+        // print!("{}", self.board[l][c] & !mask);
+        // print!("F{}\n", (self.board[l][c] & mask) == mask);
+        return (self.board[l][c] & mask) == mask;
+    }
+
+
+    fn get_options_for_cell(&self, l: usize, c: usize) -> Vec<usize> {
+        // println!("A{}", l);
+        // println!("B{}", c);
+        if l >= L || c >= C || self.is_filled_square(l, c) {
             panic!("Out of bounds");
         }
-        let pos_val = self.pos[l][c];
+        let pos_val = self.board[l][c];
         let mut options = Vec::new();
-        for i in 1..=9 { // We skip 0 since it's never an option
+
+        for i in 1..=9 { // We skip 0 since it's the flag bit
             if (pos_val & (1 << (15 - i))) != 0 {
                 options.push(i);
             }
         }
         options
     }
-
-    // Removes a specific option and sets the opt array to all zeros
-    fn clear_opt_at(&mut self, l: usize, c: usize) {
-        if c < C && l < L {
-            self.pos[l][c] = 0;
-        } else {
-            panic!("Out of bounds or invalid index");
+    fn set_board_val(&mut self, l: usize, c: usize, v: usize) {
+        if v == 0 {
+            // dont set flags of empty squares
+            self.board[l][c] = 0;
+            return;
         }
+        self.board[l][c] = 0b1000000000000000 | v as u16;
     }
 
-    fn set_board(&mut self, l: usize, c: usize, v: u8) {
-        if c < C && l < L {
-            self.board[l][c] = v;
-            self.clear_opt_at(l, c);
-        } else {
-            panic!("Out of bounds or invalid index");
+    fn get_board_val(&mut self, l: usize, c: usize) -> u16{
+        if self.is_filled_square(l, c) {
+            return self.board[l][c] & 0b0111111111111111;
+        }
+        else {
+            return 0;
         }
     }
 
     fn is_full(&self) -> bool {
         for l in 0..L {
             for c in 0..C {
-                if self.board[l][c] == 0 {
+                if !self.is_filled_square(l, c) {
                     return false;
                 }
             }
@@ -69,10 +86,10 @@ impl State {
         true
     }
 
-    fn print(&self) -> bool {
+    fn print(&mut self) -> bool {
         for l in 0..L {
             for c in 0..C {
-                print!("{}", self.board[l][c]);
+                print!("{}", self.get_board_val(l ,c));
             }
             print!("\n");
         }
@@ -80,38 +97,23 @@ impl State {
         true
     }
 
-    // fn remove_option_at(&mut self, c: usize, l: usize, opt: usize) {
-    //     if c < C && l < L && opt < 10 {
-    //         let adjusted_idx = 15 - opt; // Adjust the index
-    //
-    //         // Set the specific bit to 0
-    //         self.pos[l][c] &= !(1 << adjusted_idx);
-    //
-    //         // Decrease the number of options by 1
-    //         let options_count = self.pos[l][c] & 63; // Get current count
-    //         self.pos[l][c] = (self.pos[l][c] & !63) | (options_count - 1);
-    //     } else {
-    //         panic!("Out of bounds or invalid index");
-    //     }
-    // }
-
     fn update_pos(&mut self) {
         for l in 0..L {
             for c in 0..C {
-                if self.board[l][c] == 0 { // Only update for empty cells
-                    let mut options: u16 = 0b0111111111000000; // All options from 1 to 9 available initially (15th bit is unset for option 0)
+                if !self.is_filled_square(l, c) { // Only update for empty cells
+                    let mut options: u16 = 0b0111111111000000; // All options from 1 to 9 available initially (15th bit is unset as flag)
 
                     // Check row
                     for col in 0..C {
-                        if self.board[l][col] != 0 {
-                            options &= !(1 << (15 - self.board[l][col] as usize));
+                        if self.is_filled_square(l, col) {
+                            options &= !(1 << (15 - (self.board[l][col] & 0b0111111111111111) as usize));
                         }
                     }
 
                     // Check column
-                    for row in 0..L {
-                        if self.board[row][c] != 0 {
-                            options &= !(1 << (15 - self.board[row][c] as usize));
+                    for lin in 0..L {
+                        if self.is_filled_square(lin, c) {
+                            options &= !(1 << (15 - (self.board[lin][c] & 0b0111111111111111) as usize));
                         }
                     }
 
@@ -120,8 +122,8 @@ impl State {
                     let grid_start_col = (c / 3) * 3;
                     for row in grid_start_row..grid_start_row + 3 {
                         for col in grid_start_col..grid_start_col + 3 {
-                            if self.board[row][col] != 0 {
-                                options &= !(1 << (15 - self.board[row][col] as usize));
+                            if self.is_filled_square(row, col) {
+                                options &= !(1 << (15 - (self.board[row][col] & 0b0111111111111111) as usize));
                             }
                         }
                     }
@@ -132,7 +134,7 @@ impl State {
                     // Clear the last 6 bits and then set them to represent the number of available options
                     options = (options & 0xFFC0) | num_options;
 
-                    self.pos[l][c] = options;
+                    self.board[l][c] = options;
                 }
             }
         }
@@ -142,26 +144,32 @@ impl State {
     fn clone(&self) -> Self {
         Self {
             board: self.board.clone(),
-            pos: self.pos.clone(),
         }
     }
 }
 
 fn main() {
     let mut begin_state = State {
-        board: [
-            [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9],
-        ],
-        pos: [[0; C]; L],
+        board:[[0; C]; L],
     };
+
+    let board: [[u16; C]; L] =  [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+    ];
+
+    for l in 0..L {
+        for c in 0..C {
+            begin_state.set_board_val(l, c, board[l][c] as usize);
+        }
+    }
 
     begin_state.update_pos();
 
@@ -174,7 +182,7 @@ fn main() {
         for v in cur.get_options_for_cell(l, c) {
             let mut new_state = cur.clone();
 
-            new_state.set_board(l, c, v);
+            new_state.set_board_val(l, c, v);
 
             if new_state.is_full() {
                 new_state.print();
